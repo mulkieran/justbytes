c953ee60c7d4d0575363fb5b697c87ea9c7d89d8
Carmine Zaccagnino <zaccagnino@hotmail.it>
2017-12-09 15:43:30 +0100

    Maximum acceptable justbases version set to 0.9

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setuptools.setup(
         'Topic :: System :: Operating System Kernels :: Linux',
     ],
     install_requires = [
-        'justbases>0.07',
+        'justbases>0.07,<=0.9',
         'six'
     ],
     package_dir={"": "src"},
