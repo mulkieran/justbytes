50520fc02b2ca97ab30bb7807f854ae249e96d84
mulhern <amulhern@redhat.com>
2021-07-26 17:49:23 -0400

    Remove no self-use diable for tests
    self is always used in tests
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- check.py
+++ check.py
@@ -29,7 +29,6 @@ arg_map = {
         "--reports=no",
         "--disable=I",
         "--disable=invalid-name",
-        "--disable=no-self-use",
         "--msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'",
     ],
 }
