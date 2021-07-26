7083ae757b9f3a1ee5ead7b61a73b8949af778f9
mulhern <amulhern@redhat.com>
2021-07-26 17:48:30 -0400

    Do not allow duplicate code
    pylint can not find any
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- check.py
+++ check.py
@@ -28,7 +28,6 @@ arg_map = {
     "tests": [
         "--reports=no",
         "--disable=I",
-        "--disable=duplicate-code",
         "--disable=invalid-name",
         "--disable=no-self-use",
         "--msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'",
