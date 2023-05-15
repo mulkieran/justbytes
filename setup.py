9c84b089fa6312c4618dd9bb555dc1afda415696
mulhern <amulhern@redhat.com>
2023-05-15 09:21:47 -0400

    Move setup.py contents into setup.cfg
    Signed-off-by: mulhern <amulhern@redhat.com>

--- setup.py
+++ setup.py
@@ -20,52 +20,7 @@
 Python packaging file for setup tools.
 """
 
-# isort: STDLIB
-import os
-
 # isort: THIRDPARTY
 import setuptools
 
-
-def local_file(name):
-    """
-    Function to obtain the relative path of a filename.
-    """
-    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
-
-
-README = local_file("README.rst")
-
-with open(local_file("src/justbytes/version.py"), encoding="utf-8") as o:
-    exec(o.read())  # pylint: disable=exec-used
-
-with open(local_file("README.rst"), encoding="utf-8") as o:
-    long_description = o.read()
-
-setuptools.setup(
-    name="justbytes",
-    version=__version__,  # pylint: disable=undefined-variable
-    url="http://pythonhosted.org/justbytes/",
-    author="Anne Mulhern",
-    author_email="amulhern@redhat.com",
-    description="computing with and displaying bytes",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    platforms=["Linux"],
-    license="LGPL-2.1-or-later",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: System :: Hardware",
-        "Topic :: System :: Operating System Kernels :: Linux",
-    ],
-    install_requires=["justbases>=0.13"],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages("src"),
-)
+setuptools.setup()
